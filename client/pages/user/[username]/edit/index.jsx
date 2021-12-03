5f52032beb8888cdb9680e7e634c7bfabeec9138
Hoang Long <71806715+someseconds1402@users.noreply.github.com>
2021-12-03 20:06:59 +0700

    user: edit

--- client/pages/user/[username]/edit/index.jsx
+++ client/pages/user/[username]/edit/index.jsx
@@ -1,16 +1,35 @@
+import React, { useState } from 'react';
+import { Tabs } from 'antd';
 import FRLayout from '../../../../layouts/FRLayout';
-import { Input } from 'antd';
+import ProfileEdit from './ProfileEdit';
+import PasswordChange from './PasswordChange';
+import items from '../../../../testcategories';
+
+const { TabPane } = Tabs;
+
 
 const UserEdit = () => {
+  const [userInfo, setUserInfo] = useState(items);
   return (
-    <FRLayout>
-      <div className='w-3/4 mx-auto'>
-        <h1>Edit</h1>
-        <input type='text' placeholder='Edit username' />
-        <Input />
-      </div>
-    </FRLayout>
+    <>
+      <FRLayout>
+        <div className='w-3/4 mx-auto border rounded-md m-8' style={{ height: 900 }}>
+          <div className='m-6' >
+            <Tabs tabPosition='left'>
+              <TabPane tab="Chỉnh Sửa Profile" key="1">
+                <ProfileEdit userInfo={userInfo[0]} />
+              </TabPane>
+              <TabPane tab="Đổi Mật Khẩu" key="2">
+                <PasswordChange />
+              </TabPane>
+            </Tabs>
+          </div>
+        </div>
+      </FRLayout>
+
+    </>
   );
 };
+
 
 export default UserEdit;
